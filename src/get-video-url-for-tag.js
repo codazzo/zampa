5b2afb5598f8d9c2cc80c0ec10744c2dec46d319
Antonio D'Ettole <codazzo@gmail.com>
2017-02-28 22:40:45 +0100

    Add option to use local API proxy to fetch videos data

--- src/get-video-url-for-tag.js
+++ src/get-video-url-for-tag.js
@@ -1,14 +1,27 @@
 const getYoutubeURLFromID = id => `https://www.youtube.com/watch?v=${id}`;
 
-export default function getVideoUrlForTag(tag) {
-  const videosUrl = tag.track.content.videos.href;
-  const proxiedUrl = `https://jsonp.afeld.me/?callback=?&url=${videosUrl}`;
+const fetchResponseFromLocalProxy = (videosUrl) => {
+  const path = videosUrl.split('cdn.shazam.com/')[1];
+  const proxiedUrl = `http://localhost:3001/${path}`;
+  return fetch(proxiedUrl)
+    .then(res => res.json());
+};
 
+const fetchResponseFromRemoteProxy = (videosUrl) => { // eslint-disable-line no-unused-vars
+  const proxiedUrl = `https://jsonp.afeld.me/?callback=?&url=${videosUrl}`;
   return fetch(proxiedUrl)
     .then(res => res.text())
     .then((res) => {
       const json = res.substr(2, res.length - 4);
-      const obj = JSON.parse(json);
+      return JSON.parse(json);
+    });
+};
+
+export default function getVideoUrlForTag(tag) {
+  const videosUrl = tag.track.content.videos.href;
+
+  return fetchResponseFromLocalProxy(videosUrl)
+    .then((obj) => {
       const videos = obj.youtube.videos;
       const firstVideo = videos[0];
 
